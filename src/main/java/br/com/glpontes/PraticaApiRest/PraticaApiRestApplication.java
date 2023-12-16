55cc97afa101c531b2eb01cce450bb45c0f989eb
Gabriel Leandro Pontes da Silva <gabriel.pontes@dcx.ufpb.br>
2023-12-16 10:23:39 -0300

    Configurando as dependências

---
+++ src/main/java/br/com/glpontes/PraticaApiRest/PraticaApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.glpontes.PraticaApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaApiRestApplication.class, args);
+	}
+
+}
